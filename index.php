8bb2aab93a52eb2665375ea2485b7f76e271bbf3
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-05-04 16:12:27 +0200

    add table whit info hotels

--- index.php
+++ index.php
@@ -1,3 +1,59 @@
+<?php
+
+/*
+Stampare tutti i nostri hotel con tutti i dati disponibili.
+
+Iniziate per steps:
+Prima stampate in pagina i dati, senza preoccuparvi dello stile.
+Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.
+
+Bonus: 1
+Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
+
+
+Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
+NOTA: deve essere possibile utilizzare entrambi i filtri contemporaneamente (es. ottenere una lista con hotel che dispongono di parcheggio e che hanno un voto di tre stelle o superiore)
+Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gli hotel.
+*/
+$hotels = [
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,12 +61,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php Hotel</title>
+    <title>Php Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
 
-
+    <table class="m-5">
+        <thead>
+            <tr class="bg-danger">
+                <th>Hotel Name</th>
+                <th>Description</th>
+                <th>Parking</th>
+                <th>Rating</th>
+                <th>Distance to Center</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($hotels as $hotel) : ?>
+                <tr>
+                    <?php foreach ($hotel as $info) : ?>
+                        <td class="text-center border">
+                            <?php if ($info !== true && $info !== false) {
+                                echo $info;
+                            } elseif ($info === true) {
+                                echo "Yes";
+                            } else {
+                                echo "No";
+                            } ?>
+                        </td>
+                    <?php endforeach; ?>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </body>
 
 </html>
